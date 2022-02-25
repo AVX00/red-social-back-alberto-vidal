e452af02cdda8560b763d91717da319946781b54
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-25 21:07:09 +0100

    moved express app to server index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const serverSays = require("debug")("social:root:");
 const chalk = require("chalk");
-const express = require("express");
-const raiseServer = require("./raiseServer");
+const raiseServer = require("./server/raiseServer");
+const app = require("./server/index");
 
-const app = express();
+const port = process.env.PORT || 3000;
 
 (async () => {
   try {
-    await raiseServer(app, 3000);
+    await raiseServer(app, port);
   } catch (error) {
     serverSays(
       chalk.red(error.code === "EADDRINUSE" ? "port busy" : error.message)
--- src/server/raiseServer.js
+++ src/server/raiseServer.js
@@ -4,10 +4,6 @@ const serverSays = require("debug")("social:server:");
 
 const raiseServer = async (app, port) =>
   new Promise((resolve, reject) => {
-    app.use((req, res) => {
-      res.json({ taworkin: true });
-    });
-
     const server = app.listen(port, () => {
       serverSays(
         chalk.bgGreen.black(`server listening at http://localhost:${port}`)
