4e6cf5b63d47e3e715ec35c13fdfed81bf5b58ab
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-26 17:03:32 +0100

    fixed express security hotspot

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { default: helmet } = require("helmet");
 const userRouter = require("./routers/userRouter");
 
 const app = express();
 
+app.use(helmet());
 app.use("/user", userRouter);
 
 module.exports = app;
