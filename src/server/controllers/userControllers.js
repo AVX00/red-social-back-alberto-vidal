f83c8d4df5a401d8b75ae52870500edfe3098b50
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-26 15:05:04 +0100

    refactored register into a controller at controllers folder

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,13 @@
+const registerUser = async (req, res, next) => {
+  const { username, name, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({username, name , password: hashedPassword })
+    res.status(201).json({user: `${username} created`})
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {registerUser}
--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../../database/models/User");
+const { registerUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", async (req, res, next) => {
-  const { username, name, password } = req.body;
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({username, name , password: hashedPassword })
-    res.status(201).json({user: `${username} created`})
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/register", registerUser);
