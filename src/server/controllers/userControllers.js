22729677bba6b1d380f340ad981ccebeb27ee25a
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-26 15:20:16 +0100

    added a failing test

--- src/server/controllers/userControllers.js
+++ src/server/controllers/userControllers.js
@@ -1,13 +1,16 @@
+const bcrypt = require("bcrypt");
+const User = require("../../database/models/User");
+
 const registerUser = async (req, res, next) => {
   const { username, name, password } = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({username, name , password: hashedPassword })
-    res.status(201).json({user: `${username} created`})
+    await User.create({ username, name, password: hashedPassword });
+    res.status(201).json({ user: `${username} created` });
   } catch (error) {
     next(error);
   }
-}
+};
 
-module.exports = {registerUser}
+module.exports = { registerUser };
