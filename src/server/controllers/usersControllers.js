f1d22d72ecaa246b042788e0c30b20824dab3dfa
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-01 12:04:41 +0100

    added friends and enemies routes and controllers

--- src/server/controllers/usersControllers.js
+++ src/server/controllers/usersControllers.js
@@ -9,4 +9,24 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
-module.exports = getAllUsers;
+const getFriends = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const { friends } = await User.findById(id).populate("friends");
+    res.json(friends);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getEnemies = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const { enemies } = await User.findById(id).populate("enemies");
+    res.json(enemies);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllUsers, getFriends, getEnemies };
